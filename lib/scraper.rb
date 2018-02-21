b88aa52942ef8053bf69f814346ca95077bc5d27
Jay Bednar <ironviralmedia@gmail.com>
2018-02-21 18:05:05 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,14 +7,18 @@ class Scraper
   
     def print_courses
     self.make_courses
-    Course.all.each do |course|
-      if course.title
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
+      Course.all.each do |course|
+        if course.title
+          puts "Title: #{course.title}"
+          puts "  Schedule: #{course.schedule}"
+          puts "  Description: #{course.description}"
+        end
       end
     end
-  end
+
+    def get_page 
+
+    end 
   
 end
 
